2c9f1589b08b872c073ef56fa26dd2f6df60077a
Yash Morya <73687758+YashMorya@users.noreply.github.com>
2021-02-21 12:46:57 +0530

    Update __init__.py

--- LightYagami/__init__.py
+++ LightYagami/__init__.py
@@ -90,6 +90,7 @@ if ENV:
     SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
     SPAMWATCH_API = os.environ.get('SPAMWATCH_API', None)
+    REPOSITORY = os.environ.get("REPOSITORY", "")
   
 
     try:

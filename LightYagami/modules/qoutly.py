20d4b8056c5e0c4aaa9b3d015dc55ed706084756
Yash Morya <73687758+YashMorya@users.noreply.github.com>
2021-02-22 22:52:09 +0530

    Update qoutly.py

--- LightYagami/modules/qoutly.py
+++ LightYagami/modules/qoutly.py
@@ -1,4 +1,6 @@
   
+# Oringinal Source from Nicegrill: https://github.com/erenmetesar/NiceGrill/
+# Ported to Lynda by: @pokurt
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import types, functions
 from fontTools.ttLib import TTFont 
@@ -11,8 +13,7 @@ import random
 import json
 import os
 import re
-
-from LightYagami.events import register
+from LightYagami.igris import igrisbot
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
@@ -374,7 +375,7 @@ async def replied_user(draw, tot, text, maxlength, title):
                 draw.text((180 + space, 132), letter, font=textfont, fill="white")
                 space += textfont.getsize(letter)[0]
                 
-@register(pattern="^/q")
+@igrisbot(pattern="^/q")
 async def _(event):
     if event.fwd_from:
         return

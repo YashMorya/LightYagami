e96d20cd06d7a6d5acfe20fa8a7b3c6d013e45b9
Yash Morya <73687758+YashMorya@users.noreply.github.com>
2021-02-22 23:12:09 +0530

    Update meme.py

--- LightYagami/modules/meme.py
+++ LightYagami/modules/meme.py
@@ -123,7 +123,7 @@ def slap(update, context):
     reply_text(repl, parse_mode=ParseMode.MARKDOWN)
 
 
-#sanitize a user - by @LightYagami
+#sanitize a user - by @saitamarobot
 @run_async
 @typing_action
 def sanitize(update: Update, context: CallbackContext):
@@ -491,36 +491,41 @@ def goodmorning(update, context):
 
 __help__ = """
 *Some dank memes for fun or whatever!*
- ✪ `/sanitize`: Sanitize Your Self
- ✪ `/shrug` or `/cri`: Get shrug or ToT.
- ✪ `/decide`: Randomly answer yes no etc.
- ✪ `/abuse`: Abuses the retard!
- ✪ `/table`: Flips a table...
- ✪ `/runs`: Reply a random string from an array of replies.
- ✪ `/slap`: Slap a user, or get slapped if not a reply.
- ✪ `/pasta`: Famous copypasta meme, try and see.
- ✪ `/clap`: Claps on someones message!
- ✪ `/owo`: UwU-fy whole text XD.
- ✪ `/roll`: Rolls a dice	
- ✪ `/recite`: Logical quotes to change your life.
- ✪ `/stretch`:  streeeeeeetch iiiiiiit.
- ✪ `/hug`: Hug a user warmly, or get hugged if not a reply.
- ✪ `/pat`: pats a user, or get patted
- ✪ `/shou`t: write anything you want to give loud shoute
- ✪ `/plet <text>`: make ur text sticker in different colours
+ ✪ /sanitize*:* Sanitize Your Self
+ ✪ /shrug or /cri*:* Get shrug or ToT.
+ ✪ /decide*:* Randomly answer yes no etc.
+ ✪ /abuse*:* Abuses the retard!
+ ✪ /table*:* Flips a table...
+ ✪ runs*:* Reply a random string from an array of replies.
+ ✪ /slap*:* Slap a user, or get slapped if not a reply.
+ ✪ /pasta*:* Famous copypasta meme, try and see.
+ ✪ /clap*:* Claps on someones message!
+ ✪ /owo*:* UwU-fy whole text XD.
+ ✪ /roll*:* Rolls a dice	
+ ✪ /recite*:* Logical quotes to change your life.
+ ✪ /stretch*:*  streeeeeeetch iiiiiiit.
+ ✪ /hug*:* Hug a user warmly, or get hugged if not a reply.
+ ✪ /pat*:* pats a user, or get patted
+ ✪ /shout*:* write anything you want to give loud shoute
+ ✪ /plet <text>*:* make ur text sticker in different colours
  
- ✪ `/truth` or `/dare`: Send random truth or dare.
+ ✪ /truth or /dare*:* Send random truth or dare.
+
 *Memes*
-✪ `/hitler`: Quote a message and type this command to make a caption of hitler
-✪ `/mock`: Does the same as /hitler but spongemock instead
-✪ `/kim`: Does the same as /hitler but with Kim Jong Un instead (O no plox dont bomb my house)
-✪ `/rmeme`: Sends random meme scraped from reddit
+✪ /hitler*:* Quote a message and type this command to make a caption of hitler
+✪ /mock*:* Does the same as /hitler but spongemock instead
+✪ /kim*:* Does the same as /hitler but with Kim Jong Un instead (O no plox dont bomb my house)
+✪ /rmeme*:* Sends random meme scraped from reddit
 *Regex based memes:*
-✪ `/decide` can be also used with regex like: `KIRA? <question>: randomly answer "Yes, No" etc.`
+
+✪ /decide can be also used with regex like: `suzuya? <question>: randomly answer "Yes, No" etc.`
+
 Some other regex filters are:
 `goodmorning`, `good morning` or `goodnight`, `good night`.
-Saber will reply random strings accordingly when these words are used!
+
+Suzuya will reply random strings accordingly when these words are used!
 All regex filters can be disabled incase u don't want... like: `/disable goodnight`.
+
 """
 
 __mod_name__ = "Memes"
@@ -534,7 +539,7 @@ TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize)
 SHRUG_HANDLER = DisableAbleCommandHandler("shrug", shrug)
 DECIDE_HANDLER = DisableAbleMessageHandler(
-    Filters.regex(r"(?i)^Kira\?"), decide, friendly="decide"
+    Filters.regex(r"(?i)^Suzuya\?"), decide, friendly="decide"
 )
 ABUSE_HANDLER = DisableAbleCommandHandler("abuse", abuse,pass_args=True)
 RUNS_HANDLER = DisableAbleCommandHandler("runs", runs ,pass_args=True)

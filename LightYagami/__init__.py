aea7d178c41bfaf2716879ba11a27d6981a5f73b
Yash Morya <73687758+YashMorya@users.noreply.github.com>
2021-02-28 00:38:55 +0530

    Update __init__.py

--- LightYagami/__init__.py
+++ LightYagami/__init__.py
@@ -205,7 +205,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from SaitamaRobot.modules.helper_funcs.handlers import (
+from LightYagami.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,

725263ff2bf32b29a92e522986268d072abbe83d
Yash Morya <73687758+YashMorya@users.noreply.github.com>
2021-03-01 20:52:34 +0530

    Update sample_config.py

--- LightYagami/sample_config.py
+++ LightYagami/sample_config.py
@@ -59,6 +59,7 @@ class Config(object):
     AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 
 
 class Production(Config):

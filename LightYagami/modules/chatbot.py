c49cc9e50d836707f13d3f35e39bd826f72c6fab
Yash Morya <73687758+YashMorya@users.noreply.github.com>
2021-04-27 22:45:34 +0530

    Update chatbot.py

--- LightYagami/modules/chatbot.py
+++ LightYagami/modules/chatbot.py
@@ -1,6 +1,6 @@
-from KURUMIBOT import pbot, arq, BOT_ID, DRAGONS
-from KURUMIBOT.utils.errors import capture_err
-from KURUMIBOT.utils.filter_groups import chatbot_group
+from LightYagami import pbot, arq, BOT_ID, DRAGONS
+from LightYagami.utils.errors import capture_err
+from LightYagami.utils.filter_groups import chatbot_group
 from pyrogram import filters
 
 
